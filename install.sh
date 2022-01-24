dbb7ffcc6031c7499f411fc14522b927be19ab83
Josh Spicer <josh@joshspicer.com>
2022-01-24 15:37:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ set -e
 
 # The install.sh script is the installation entrypoint for any features in this repository. 
 #
-# The tooling will parse the features.json + user devcontainer, and write 
-# any build-time arguments into a feature-set scoped "features.env"
+# The tooling will parse the devcontainer-features.json + user devcontainer, and write 
+# any build-time arguments into a feature-set scoped "devcontainer-features.env"
 # The author is free to source that file and use it however they would like.
 set -a
-. ./features.env
+. ./devcontainer-features.env
 set +a
 
 # Build args are exposed to this entire feature set following the pattern:  _BUILD_ARG_<FEATURE ID>_<OPTION NAME>
